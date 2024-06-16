9e5906ebf5161f943732839900635690b9e3354e
Taavi Täht <taavi656@gmail.com>
2024-06-16 19:45:24 +0300

    local storage

--- client.js
+++ client.js
@@ -1,5 +1,89 @@
 // Client js
 
+
+// On page load fetch all news items and display
+function displayAllRss() {
+    const allFeeds = getAllStoredFeeds()
+    allFeeds.forEach(feed => {
+        fetchRss(feed.storedRssFeed)
+    })
+}
+
+// Local storage for rss feeds
+// Initialize if nothing in local storage (run automatically)
+if (localStorage.length == 0) {
+    localStorage.setItem('rss1', 'https://flipboard.com/@raimoseero/feed-nii8kd0sz.rss');
+    for (let i = 2; i <= 20; i++) {
+        localStorage.setItem(`rss${i}`, '');
+    }
+}
+
+// The user can add/edit/remove custom RSS feeds, which should remain even after refreshing the page
+function displayFeeds() {
+    // Display stored feeds on modal
+    const modalDiv = document.getElementById('modal-div')
+    //modalDiv.innerText = ""
+
+    const allFeeds = getAllStoredFeeds()
+    let htmlOutput = `Stored RSS feeds<br><br>`;
+
+    // List all stored feeds
+    allFeeds.forEach(feed => {
+        htmlOutput += ` 
+            <li class="storedFeed"> 
+                <input type="text" id="${feed.key}" value="${feed.storedRssFeed}">
+                <button onclick="">Save</button>
+                <br><br>
+            </li> 
+        `;
+    });
+    // Add empty field for adding new feed
+    let empty = getFirstEmptyStorage
+    htmlOutput += `<li class="storedFeed"> 
+                <input type="text" id="${empty.key}" placeholder="Add another..."><br><br>
+            </li> `
+    modalDiv.innerHTML = htmlOutput;
+
+
+    modal.showModal();
+}
+
+// Find first empty local storage container
+function getFirstEmptyStorage() {
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        // Check key
+        if (key.startsWith('rss')) {
+            const storedRssFeed = localStorage.getItem(key);
+            // Check if has stored content
+            if (!storedRssFeed) {
+                console.log('First empty slot: ', localStorage.key)
+                return localStorage.key
+            }
+        }
+    }
+}
+
+// Get all saved rss feeds from local storage
+function getAllStoredFeeds() {
+    const rssArr = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        // Check key
+        if (key.startsWith('rss')) {
+            const storedRssFeed = localStorage.getItem(key);
+            // Check if has stored content
+            if (storedRssFeed) {
+                rssArr.push({ key, storedRssFeed });
+            }
+        }
+    }
+    console.log(rssArr)
+    return rssArr;
+}
+
+
+
 // Function to get RSS feed from the local proxy server
 async function fetchRss(url) {
 
@@ -17,6 +101,7 @@ async function fetchRss(url) {
         const parser = new DOMParser();
         const xml = parser.parseFromString(text, "application/xml");
         //return xml;
+        // Go on to display fetched rss feen on page
         displayRss(xml)
     } catch (error) {
         console.error('Error fetching the RSS feed:', error);
@@ -31,17 +116,17 @@ function displayRss(xml) {
     items.forEach(itemElement => {
         htmlOutput += ` 
             <li class="articlePreview"> 
-                <h3>                                                
-                    <a href="${itemElement.querySelector("link").innerHTML}" target="_blank" rel="noopener">                                 
-                        ${itemElement.querySelector("title").innerHTML} 
-                        <button style="border-radius:8px; background-color:rgb(32, 94, 170); color:white;border:none"> 
-                            RSS 
-                        </button> 
-                    </a>                             
+                <h3 class="clickable">                                                
+                                               
+                    <p onclick="displayArticle('${itemElement.querySelector("link").innerHTML}')">                                 
+                        ${itemElement.querySelector("title").innerHTML}
+                    </p>                             
                 </h3> 
-                <p> 
-                    ${itemElement.querySelector("description").innerHTML} 
-                </p>          
+                <div class="clickable">
+                    <p onclick="displayArticle('${itemElement.querySelector("link").innerHTML}')"> 
+                        ${itemElement.querySelector("description").innerHTML} 
+                    </p> 
+                </div>         
                 <p class="publishedOnBy">
                     Published on ${itemElement.querySelector("pubDate").innerHTML} by ${itemElement.querySelector("author").innerHTML} 
                 </p>               
@@ -58,39 +143,33 @@ function displayRss(xml) {
 // Modal functionality
 const modal = document.getElementById('modal');
 const openButton = document.querySelector('.open-button');
-const closeButton = document.querySelector('.close-button');
 
-// Define an async function to handle decluttering
-async function handleOpenButtonClick() {
+// Display article on modal
+async function displayArticle(url) {
+    // Open modal
+    document.getElementById('modal-div').innerText = "Loading article..."
+    modal.showModal();
+    // Fetch news article for displaying
     try {
-        modal.showModal();
-        let article = await declutter('https://www.helpnetsecurity.com/2024/05/22/authelia-open-source-authentication-authorization-server/');
+        let article = await declutter(url);
         document.getElementById('modal-div').innerHTML = article.content;
     } catch (error) {
         console.error('Error fetching and displaying article:', error);
-        // Handle error, e.g., display an error message
+        document.getElementById('modal-div').innerText = ('Error fetching and displaying article:', error);
     }
 }
 
-// Attach event listener to openButton
-openButton.addEventListener('click', handleOpenButtonClick);
-
-closeButton.addEventListener('click', () => {
-    modal.close();
-});
-
+// Close modal on click outside of modal
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.close();
     }
 }
 
-
-
 // Before displaying the article, it must be freed from clutter using the Mercury API web parser
 async function declutter(url) {
 
-    if (!url) { url = 'https://www.theverge.com/tech'}  // Default url
+    if (!url) { url = 'https://www.theverge.com/tech' }  // Default url
     //const apiUrl = 'http://0.0.0.0:3000/webparser';   // Local server proxy for webparser
     const apiUrl = 'https://lolo-slse.onrender.com/webparser'   // For running on render
 
@@ -114,5 +193,3 @@ async function declutter(url) {
         console.error('Error sending POST request:', error);
     }
 }
-// Call the function to send the POST request
-//declutter('https://www.theverge.com/tech');
